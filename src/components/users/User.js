ded4e4f31a0d33bd248c9ff2d56f0aff18bb2bc1
Patrick Pruß <pruss.patrick@googlemail.com>
2019-06-16 18:22:12 +0200

    Refactored all props with context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,12 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
-import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+import GithubContext from '../../context/github/githubContext'
 
-const User = ({user, loading, repos, getUser, getUserRepos, match}) => {
+const User = ({match}) => {
+
+    const {repos, getUserRepos, getUser, loading, user} = useContext(GithubContext);
 
     useEffect(() => {
         getUser(match.params.login);
@@ -74,14 +76,6 @@ const User = ({user, loading, repos, getUser, getUserRepos, match}) => {
             <Repos repos={repos} />
         </Fragment>
     )
-}
-
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired
 }
 
 export default User
